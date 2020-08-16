4f28044bdd9cfc369df72d54ebaa7f44b6bdb100
Samuel Chevalier <samuelchevalier@laposte.net>
2020-08-16 15:26:38 +0200

    Way to first release

--- Publish-Profiles.ps1
+++ Publish-Profiles.ps1
@@ -1,1 +1,11 @@
-git remote -v
+$devDir      = 'Z:\Samuel\star citizen\HCS Translation'
+$prodDir     = 'C:\Users\Samuel\AppData\Roaming\VoiceAttack\HCS VoicePacks\Custom VoiceTriggers'
+$profileName = 'French Event Horizon'
+
+Clear-Host
+
+Copy-Item "$($prodDir)\$($profileName).hcs" -Destination "$($devDir)\backups\$($profileName).hcs.$(Get-Date -Format 'yyyyMMddHHmmss')" | Out-Null
+
+Copy-Item "$($devDir)\$($profileName).hcs"  -Destination "$($prodDir)\$($profileName).hcs" -Force | Out-Null
+
+git add *;git commit -am "Way to first release"; git push
